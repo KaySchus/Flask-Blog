3fde4a9b7f112932c089c232512af4fc5e8d9d04
Kaleb Schuster <ksschu06@louisville.edu>
2017-05-24 14:39:29 -0400

    Layout sample only - no functionality integrated

--- flaskblog/assets.py
+++ flaskblog/assets.py
@@ -1,6 +1,8 @@
 from flask_assets import Bundle
 
 common_css = Bundle(
-	'css/vendor/pure.min.css',
+	'css/vendor/pure-min.css',
+	'css/vendor/grids-responsive-min.css',
 	filters='cssmin',
+	output='public/css/common.css'
 )
