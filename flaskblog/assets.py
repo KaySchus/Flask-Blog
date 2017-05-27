74223234f67fc9ab8d787b464e39e70f4a83ff98
Kaleb Schuster <ksschu06@louisville.edu>
2017-05-27 16:53:03 -0400

    Template shuffling

--- flaskblog/assets.py
+++ flaskblog/assets.py
@@ -3,6 +3,7 @@ from flask_assets import Bundle
 common_css = Bundle(
 	'css/vendor/pure-min.css',
 	'css/vendor/grids-responsive-min.css',
+	'css/sidebar.css',
 	filters='cssmin',
 	output='public/css/common.css'
 )
